1bc3fd49c7d3f8e987b969f83c099cc32baadc76
FC\abm <a.m@ckay.me>
2020-08-02 20:37:11 +0100

    debug doc generation

--- build/publish-docs.ps1
+++ build/publish-docs.ps1
@@ -26,6 +26,8 @@ if (-not ($canPublishDocs -and ($isReleasePublication -or $forceDocPush)))
 
 Push-Location (Join-Path $env:APPVEYOR_BUILD_FOLDER "../fireflycons.github.io")
 
+gci Firefly-CloudFormation/api
+
 try
 {
     # Stage any changes, suppressing line ending warnings (nearly all at least)
